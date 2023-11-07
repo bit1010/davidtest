073a52ce49ce1ce570724cfbaec6c6193011ccb4
shinyoung.joo <bit1010@live.com>
2023-11-07 15:11:28 +0900

    1

--- main.py
+++ main.py
@@ -24,5 +24,9 @@ def hello_world():
 def menu():
     return render_template('menu.html')
 
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
