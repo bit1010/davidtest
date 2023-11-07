076554d35915934cc82b08603ce4346ff04872f9
bit1010 <bit1010@live.com>
2023-11-07 15:22:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,5 +24,9 @@ def hello_world():
 def menu():
     return render_template('menu.html')
     
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+    
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
